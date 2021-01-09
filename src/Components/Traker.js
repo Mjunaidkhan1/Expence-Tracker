f4ab22deb38753f9b20e77edf9c4826516129a60
Mjunaidkhan1 <jk8127368@gmail.com>
2021-01-09 02:37:57 -0800

    add style

--- src/Components/Traker.js
+++ src/Components/Traker.js
@@ -10,8 +10,8 @@ export default function Traker() {
 
     const saveData = (e) => {
         e.preventDefault()
-        // console.log(amount)
-        if(Number(amount)===0){
+
+        if (Number(amount) === 0) {
             alert('CORRECT VALUE')
             return false
         }
@@ -19,78 +19,83 @@ export default function Traker() {
             amoun: Number(amount),
             dec: data
         })
+        setData('')
+        setAmount('')
     }
 
     const getIncom = () => {
         let Incom = 0
         for (var i = 0; i < transec.length; i++) {
-            if (transec[i].amount > 0) 
+            if (transec[i].amount > 0)
                 Incom = Incom + transec[i].amount
-            
-                
-            }
-            return Incom
+
+
+        }
+        return Incom
     }
 
     const getExpence = () => {
-        let Expence= 0
+        let Expence = 0
         for (var i = 0; i < transec.length; i++) {
-            if (transec[i].amount < 0) 
+            if (transec[i].amount < 0)
                 Expence = Expence + transec[i].amount
-        
-                
-            }
-            return Expence
+
+
+        }
+        return Expence
     }
 
     return (
-        <div className='Contaner'>
-            <h1>expence tracker</h1>
+        <div>
+            <div className='Contaner'>
+                <h1>expence tracker</h1>
 
-            <h3>your balance <br /> <span>{getIncom() + getExpence()}</span> </h3>
+                <h3>your balance <br /> <span>{getIncom() + getExpence()}</span> </h3>
 
-            <div className='Expen'>
+                <div className='Expen'>
 
-                <h3>INCOME <br /> <span>{getIncom()}</span> </h3>
-                <h3>expence <br /> <span>{getExpence()}</span> </h3>
+                    <h3>INCOME <br /> <span>{getIncom()}</span> </h3>
+                    <h3>expence <br /> <span>{getExpence()}</span> </h3>
+                </div>
+                <h2>history</h2>
+                <hr />
+
+                <ul className='ul'>
+                    {transec.map((data, ind) => {
+
+
+                        return (
+                            <li key={ind}>
+                                <span>{data.des}</span>
+                                <span>{data.amount}</span>
+                            </li>
+                        )
+
+                    })}
+
+
+                </ul>
+
+                <h2>add transection</h2>
+                <hr />
+                <form className='form' onSubmit={saveData}>
+                    <label>
+                        text
+                    <br />
+                        <input value={data} placeholder='write text' type='text' required onChange={(e) => setData(e.target.value)} />
+                    </label>
+
+                    <br />
+                    <label>
+                        amount
+                    <br />
+                        <input value={amount} placeholder='write amount' type='number' required onChange={(e) => setAmount(e.target.value)} />
+                    </label>
+                    <br />
+                    <button className='button'>add</button>
+                </form>
             </div>
-            <h2>history</h2>
-            <hr />
-
-            <ul className='ul'>
-                {transec.map((data, ind) => {
-                    console.log(data)
-                    return (
-                        <li>
-                            <span>{data.des}</span>
-                            <span>{data.amount}</span>
-                        </li>
-                    )
-                })}
-
-
-            </ul>
-
-            <h2>add transection</h2>
-            <hr />
-            <form className='form' onSubmit={saveData}>
-                <label>
-                    text
-                    <br />
-                    <input placeholder='write text' type='text' required onChange={(e) => setData(e.target.value)} />
-                </label>
-
-                <br />
-                <label>
-                    amount
-                    <br />
-                    <input placeholder='write amount' type='number' required onChange={(e) => setAmount(e.target.value)} />
-                </label>
-                <br />
-                <button className='button'>add</button>
-            </form>
-
-
+            
         </div>
     )
 }
